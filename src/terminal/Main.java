712bba7a7622946707449dca21a6f732325e4f00
mihino89 <martin.mihalovic98@gmail.com>
2020-03-22 11:02:03 +0100

    javafx basics

--- src/terminal/Main.java
+++ src/terminal/Main.java
@@ -1,36 +1,1 @@
-package terminal;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
-
-public class Main extends Application {
-    private static Menu menu;
-    Button button;
-
-    public static void main(String[] args) {
-        launch(args); // set up as javafx app
-        menu = new Menu();
-        menu.createDialog();
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-//         main javafx code
-        stage.setTitle("title of the windows");
-
-        button = new Button();
-        button.setText("Click me");
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        Scene scene = new Scene(layout, 300, 250);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-}
+package terminal;import javafx.application.Application;import javafx.scene.Scene;import javafx.scene.layout.StackPane;import javafx.stage.Stage;import javafx.scene.control.Button;public class Main extends Application {    private static Menu menu;    Button button;    public static void main(String[] args) {        launch(args); // set up as javafx app        menu = new Menu();        menu.createDialog();    }    @Override    public void start(Stage stage) throws Exception {//         main javafx code        stage.setTitle("title of the windows");        button = new Button();        button.setText("Click me");        StackPane layout = new StackPane();        layout.getChildren().add(button);        Scene scene = new Scene(layout, 300, 250);        stage.setScene(scene);        stage.show();    }}
